87763697ea2007cdd2492bd61e3b08e638e61913
Jakub Dryla <135859429+dryjak@users.noreply.github.com>
2025-07-26 15:27:57 +0200

    MOTORS LIBLARY AND INIT FUNCTION

---
+++ PrzyciskFiltrRC/Core/Src/motor_simple.c
@@ -0,0 +1,22 @@
+/*
+ * motor_simple.c
+ *
+ *  Created on: Jul 26, 2025
+ *      Author: jakub
+ */
+
+#include "main.h"
+#include "tim.h"
+#include "motor_simple.h"
+
+
+MotorsStatus_t Motors_Init(Motors_t *Motor, TIM_HandleTypeDef *timer, GPIO_TypeDef *MotorLeftPort, uint16_t MotorLeftPin, GPIO_TypeDef *MotorRightPort, uint16_t MotorRightPin)
+{
+	Motor->htim = timer;
+	Motor->MotorLeftPort 	= MotorLeftPort;
+	Motor->MotorLeftPin 	= MotorLeftPin;
+	Motor->MotorRightPort 	= MotorRightPort;
+	Motor->MotorRightPin 	= MotorRightPin;
+
+	return MOTORS_OK;
+}
--- PrzyciskFiltrRC/Debug/Core/Src/motor_simple.d
+++ PrzyciskFiltrRC/Debug/Core/Src/motor_simple.d
@@ -1,4 +1,4 @@
-Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
+Core/Src/motor_simple.o: ../Core/Src/motor_simple.c ../Core/Inc/main.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
  ../Core/Inc/stm32f4xx_hal_conf.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
@@ -27,8 +27,7 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h \
- ../Core/Inc/tim.h ../Core/Inc/main.h ../Core/Inc/usart.h \
- ../Core/Inc/gpio.h ../Core/Inc/motor_simple.h
+ ../Core/Inc/tim.h ../Core/Inc/main.h ../Core/Inc/motor_simple.h
 ../Core/Inc/main.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
 ../Core/Inc/stm32f4xx_hal_conf.h:
@@ -60,6 +59,4 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
 ../Core/Inc/tim.h:
 ../Core/Inc/main.h:
-../Core/Inc/usart.h:
-../Core/Inc/gpio.h:
 ../Core/Inc/motor_simple.h:
