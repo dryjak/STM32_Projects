cff04776498e92dd2fc9f6947398dcb2f143dd37
Jakub Dryla <135859429+dryjak@users.noreply.github.com>
2025-07-28 19:41:42 +0200

    created Motor set ride parameters and motor ride function

--- PrzyciskFiltrRC/Core/Inc/motor_simple.h
+++ PrzyciskFiltrRC/Core/Inc/motor_simple.h
@@ -30,6 +30,8 @@ typedef struct
 }Motor_t;
 
 MotorStatus_t Motor_Init(Motor_t *Motor, TIM_HandleTypeDef *Timer, uint32_t TimerChannel, uint16_t PWM, GPIO_TypeDef *Dir1Port, uint16_t Dir1Pin, GPIO_TypeDef *Dir2Port, uint16_t Dir2Pin);
+MotorStatus_t Motor_SetRideParameters(Motor_t *Motor, uint8_t PWM, uint8_t Dir);
+void Motor_Ride(Motor_t *Motor);
 
 
 #endif /* INC_MOTOR_SIMPLE_H_ */
--- PrzyciskFiltrRC/Core/Src/motor_simple.c
+++ PrzyciskFiltrRC/Core/Src/motor_simple.c
@@ -11,14 +11,32 @@
 
 #define DIR(a) ((a)>(0)?(1):(0))
 
-MotorStatus_t Motor_Ride(Motor_t *Motor)
+MotorStatus_t Motor_SetRideParameters(Motor_t *Motor, uint8_t PWM, uint8_t Dir)
 {
+	//making sure PWM is in range from 0 to 100
+	if (PWM < 0)
+	{
+		Motor->MotorPWM = -1 * PWM;
+	}
+	if (PWM > 100)
+	{
+		Motor->MotorPWM = 100;
+	}
 
+	//setting direction
 	HAL_GPIO_WritePin(Motor->MotorDir1Port, Motor->MotorDir1Pin, DIR(Motor->Direction));
 	HAL_GPIO_WritePin(Motor->MotorDir1Port, Motor->MotorDir2Pin, DIR(Motor->Direction));
-	__HAL_TIM_SET_COMPARE(Motor->htim, Motor->Channel, Motor->MotorPWM);
+
+	//setting PWM
+	//__HAL_TIM_SET_COMPARE(Motor->htim, Motor->Channel, Motor->MotorPWM);
 
 	return MOTOR_OK;
+}
+
+void Motor_Ride(Motor_t *Motor)
+{
+	//setting PWM
+	__HAL_TIM_SET_COMPARE(Motor->htim, Motor->Channel, Motor->MotorPWM);
 }
 
 MotorStatus_t Motor_Init(Motor_t *Motor, TIM_HandleTypeDef *Timer, uint32_t TimerChannel, uint16_t PWM, GPIO_TypeDef *Dir1Port, uint16_t Dir1Pin, GPIO_TypeDef *Dir2Port, uint16_t Dir2Pin)
