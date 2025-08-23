5b8d42195f25bd8a3b8e969c4c53f63ad53e0ed1
Jakub Dryla <135859429+dryjak@users.noreply.github.com>
2025-08-23 18:36:05 +0200

    basic initioalization

--- TransiverCodeNrf24V1/Core/Inc/NRF24_L1.h
+++ TransiverCodeNrf24V1/Core/Inc/NRF24_L1.h
@@ -12,6 +12,49 @@
 //#include "stm32f4xx_hal.h"
 
 
+/* Memory Map */
+#define CONFIG      0x00
+#define EN_AA       0x01
+#define EN_RXADDR   0x02
+#define SETUP_AW    0x03
+#define SETUP_RETR  0x04
+#define RF_CH       0x05
+#define RF_SETUP    0x06
+#define STATUS      0x07
+#define OBSERVE_TX  0x08
+#define CD          0x09
+#define RX_ADDR_P0  0x0A
+#define RX_ADDR_P1  0x0B
+#define RX_ADDR_P2  0x0C
+#define RX_ADDR_P3  0x0D
+#define RX_ADDR_P4  0x0E
+#define RX_ADDR_P5  0x0F
+#define TX_ADDR     0x10
+#define RX_PW_P0    0x11
+#define RX_PW_P1    0x12
+#define RX_PW_P2    0x13
+#define RX_PW_P3    0x14
+#define RX_PW_P4    0x15
+#define RX_PW_P5    0x16
+#define FIFO_STATUS 0x17
+#define DYNPD	    0x1C
+#define FEATURE	    0x1D
+
+/* Instruction Mnemonics */
+#define R_REGISTER    0x00
+#define W_REGISTER    0x20
+#define REGISTER_MASK 0x1F
+#define ACTIVATE      0x50
+#define R_RX_PL_WID   0x60
+#define R_RX_PAYLOAD  0x61
+#define W_TX_PAYLOAD  0xA0
+#define W_ACK_PAYLOAD 0xA8
+#define FLUSH_TX      0xE1
+#define FLUSH_RX      0xE2
+#define REUSE_TX_PL   0xE3
+#define NOP           0xFF
+
+
 typedef struct{
 	SPI_HandleTypeDef *NRF24_SPI;
 
@@ -22,9 +65,9 @@ typedef struct{
 	uint16_t 		NRF24CSN_Pin;
 }NRF24_t;
 
-void InitNRF24(NRF24_t *NRF24_Module, GPIO_TypeDef *NRF24CE_Port, uint16_t NRF24CE_Pin, GPIO_TypeDef *NRF24CSN_Port, uint16_t NRF24CSN_Pin, SPI_HandleTypeDef *hspi);
+void Nrf24_InitNRF24(NRF24_t *NRF24_Module, GPIO_TypeDef *NRF24CE_Port, uint16_t NRF24CE_Pin, GPIO_TypeDef *NRF24CSN_Port, uint16_t NRF24CSN_Pin, SPI_HandleTypeDef *hspi);
 
-void WriteRegister(NRF24_t *NRF24_Module, uint8_t Register, uint8_t Data);
+void Nrf24_WriteRegister(NRF24_t *NRF24_Module, uint8_t Register, uint8_t Data);
 
 
 #endif /* INC_NRF24_L1_H_ */
--- TransiverCodeNrf24V1/Core/Src/NRF24_L1.c
+++ TransiverCodeNrf24V1/Core/Src/NRF24_L1.c
@@ -8,16 +8,7 @@
 
 //extern SPI_HandleTypeDef hspi2;
 
-void InitNRF24(NRF24_t *NRF24_Module, GPIO_TypeDef *NRF24CE_Port, uint16_t NRF24CE_Pin, GPIO_TypeDef *NRF24CSN_Port, uint16_t NRF24CSN_Pin, SPI_HandleTypeDef *hspi)
-{
-	NRF24_Module->NRF24_SPI = hspi;
 
-	NRF24_Module->NRF24CE_Port = NRF24CE_Port;
-	NRF24_Module->NRF24CE_Pin = NRF24CE_Pin;
-
-	NRF24_Module->NRF24CSN_Port = NRF24CSN_Port;
-	NRF24_Module->NRF24CSN_Pin = NRF24CSN_Pin;
-}
 
 void CS_Select(NRF24_t *NRF24_Module)
 {
@@ -28,18 +19,18 @@ void CS_Unselect(NRF24_t *NRF24_Module)
 {
 	HAL_GPIO_WritePin(NRF24_Module->NRF24CSN_Port, NRF24_Module->NRF24CSN_Pin, GPIO_PIN_SET);
 }
-void CE_Select(NRF24_t *NRF24_Module)
+void CE_Enable(NRF24_t *NRF24_Module)
 {
 	HAL_GPIO_WritePin(NRF24_Module->NRF24CE_Port, NRF24_Module->NRF24CE_Pin, GPIO_PIN_RESET);
 }
-void CE_Unselect(NRF24_t *NRF24_Module)
+void CE_Disable(NRF24_t *NRF24_Module)
 {
 	HAL_GPIO_WritePin(NRF24_Module->NRF24CE_Port, NRF24_Module->NRF24CE_Pin, GPIO_PIN_SET);
 }
 
 
 //write a single byte to a particular register
-void WriteRegister(NRF24_t *NRF24_Module, uint8_t Register, uint8_t Data)
+void Nrf24_WriteRegister(NRF24_t *NRF24_Module, uint8_t Register, uint8_t Data)
 {
 	uint8_t buff[2];
 	buff[0] = Register | 1 << 5;
@@ -57,7 +48,7 @@ void WriteRegister(NRF24_t *NRF24_Module, uint8_t Register, uint8_t Data)
 
 
 //write multiple bytes to a particular register
-void WriteMultiRegister(NRF24_t *NRF24_Module, uint8_t Register, uint8_t *Data, uint8_t Size)
+void Nrf24_WriteMultiRegister(NRF24_t *NRF24_Module, uint8_t Register, uint8_t *Data, uint8_t Size)
 {
 	uint8_t buff[2];
 	buff[0] = Register | 1 << 5;
@@ -74,7 +65,7 @@ void WriteMultiRegister(NRF24_t *NRF24_Module, uint8_t Register, uint8_t *Data, 
 
 }
 
-uint8_t ReadRegister(NRF24_t *NRF24_Module, uint8_t Register)
+uint8_t Nrf24_ReadRegister(NRF24_t *NRF24_Module, uint8_t Register)
 {
 	uint8_t Data = 0;
 	//Pull cs pin low to select the device
@@ -90,7 +81,7 @@ uint8_t ReadRegister(NRF24_t *NRF24_Module, uint8_t Register)
 }
 
 
-void ReadMultipleRegister(NRF24_t *NRF24_Module, uint8_t Register, uint8_t Size, uint8_t *Data)
+void Nrf24_ReadMultipleRegister(NRF24_t *NRF24_Module, uint8_t Register, uint8_t Size, uint8_t *Data)
 {
 	//Pull cs pin low to select the device
 	CS_Select(NRF24_Module);
@@ -100,5 +91,41 @@ void ReadMultipleRegister(NRF24_t *NRF24_Module, uint8_t Register, uint8_t Size,
 
 	//Pull cs pin high to release the device
 	CS_Unselect(NRF24_Module);
+}
+
+void Nrf24_SendCommand(NRF24_t *NRF24_Module, uint8_t Cmd)
+{
+		//Pull cs pin low to select the device
+		CS_Select(NRF24_Module);
+
+		HAL_SPI_Transmit(NRF24_Module->NRF24_SPI, &Cmd, 1, 1000);
+
+		//Pull cs pin high to release the device
+		CS_Unselect(NRF24_Module);
+}
+
+void Nrf24_InitNRF24(NRF24_t *NRF24_Module, GPIO_TypeDef *NRF24CE_Port, uint16_t NRF24CE_Pin, GPIO_TypeDef *NRF24CSN_Port, uint16_t NRF24CSN_Pin, SPI_HandleTypeDef *hspi)
+{
+	NRF24_Module->NRF24_SPI = hspi;
+
+	NRF24_Module->NRF24CE_Port = NRF24CE_Port;
+	NRF24_Module->NRF24CE_Pin = NRF24CE_Pin;
+
+	NRF24_Module->NRF24CSN_Port = NRF24CSN_Port;
+	NRF24_Module->NRF24CSN_Pin = NRF24CSN_Pin;
+
+	//dissable the chip before cofiguring the device
+	CE_Disable(NRF24_Module);
+
+
+	Nrf24_WriteRegister(NRF24_Module, CONFIG, 0);			//will be configured later
+	Nrf24_WriteRegister(NRF24_Module, EN_AA, 0);			//No auto ack
+	Nrf24_WriteRegister(NRF24_Module, EN_RXADDR, 0);		//Nt enabling any data pipe now
+	Nrf24_WriteRegister(NRF24_Module, SETUP_AW, 0x03);		//5 bytes for the tx / rx address
+	Nrf24_WriteRegister(NRF24_Module, SETUP_RETR, 0x00);	//no Automatic Retransmission
+	Nrf24_WriteRegister(NRF24_Module, RF_CH, 0x00);			//will be setup with rx or tx
+	Nrf24_WriteRegister(NRF24_Module, RF_SETUP, 0x0E);			//Power = 0dB, dataRate = 2Mbps
+
+
 
 }
