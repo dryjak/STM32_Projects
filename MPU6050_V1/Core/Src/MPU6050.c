ceaeda30f07af873a0aec88833af416b539b45d6
Jakub Dryla <135859429+dryjak@users.noreply.github.com>
2025-09-10 20:13:19 +0200

    added function to read accel data from the register

--- MPU6050_V1/Core/Src/MPU6050.c
+++ MPU6050_V1/Core/Src/MPU6050.c
@@ -7,6 +7,15 @@
 #include "MPU6050.h"
 #include "main.h"
 
+typedef union{
+	struct
+	{
+		uint8_t ByteLow;
+		uint8_t ByteHigh;
+	}Byte;
+
+	uint16_t Var16u;
+}ConvTo16_t;
 
 
 HAL_StatusTypeDef MPU6050_WHO_AM_I (MPU6050_t *MPU6050, uint8_t *Who_am_I)
@@ -21,7 +30,7 @@ void MPU6050_Init(MPU6050_t *MPU6050, I2C_HandleTypeDef *Hi2c, uint16_t Address)
 }
 HAL_StatusTypeDef MPU6050_MemRead(MPU6050_t *MPU6050, uint8_t Reg, uint8_t *Data, uint16_t Size)
 {
-	return HAL_I2C_Mem_Read(MPU6050->hi2c, (MPU6050->address << 1), I2C_MEMADD_SIZE_8BIT, Size, Data, Size, HAL_MAX_DELAY);
+	return HAL_I2C_Mem_Read(MPU6050->hi2c, (MPU6050->address << 1), Reg, I2C_MEMADD_SIZE_8BIT, Data, Size, HAL_MAX_DELAY);
 }
 
 HAL_StatusTypeDef MPU6050_MemWrite(MPU6050_t *MPU6050, uint8_t Reg, uint8_t Data)
@@ -68,4 +77,31 @@ HAL_StatusTypeDef MPPU6050_SetAcceleration(MPU6050_t *MPU6050)
 
 	Register &= (~((1 << 4)|(1 << 5)));		//setting acceleration to -/+ 2g
 	return MPU6050_MemWrite(MPU6050, ACCEL_CONFIG, Register);
+}
+
+HAL_StatusTypeDef MPU6050_ReadAcceleration(MPU6050_t *MPU6050, uint16_t *Gx, uint16_t *Gy, uint16_t *Gz)
+{
+	ConvTo16_t AccelX, AccelY, AccelZ;
+	uint8_t ByteLow, ByteHigh;
+
+	uint8_t AccelData[6];
+
+	//Read Acceleration value
+	if ((MPU6050_MemRead(MPU6050, ACCEL_XOUT_L, AccelData, 6)) == HAL_ERROR)
+	{
+		return HAL_ERROR;
+	}
+
+	AccelX.Byte.ByteHigh 	= AccelData[0];
+	AccelX.Byte.ByteLow 	= AccelData[1];
+	AccelY.Byte.ByteHigh 	= AccelData[2];
+	AccelY.Byte.ByteLow 	= AccelData[3];
+	AccelZ.Byte.ByteHigh 	= AccelData[4];
+	AccelZ.Byte.ByteLow 	= AccelData[5];
+
+	*Gx = AccelX.Var16u;
+	*Gy = AccelY.Var16u;
+	*Gz = AccelZ.Var16u;
+
+	return HAL_OK;
 }
