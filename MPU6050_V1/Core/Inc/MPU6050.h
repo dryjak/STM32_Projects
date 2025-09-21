4e7c7ddadbb5d5d23d75cb915d1eef8fd8186156
Jakub Dryla <135859429+dryjak@users.noreply.github.com>
2025-09-21 21:33:39 +0200

    added gyro registers

--- MPU6050_V1/Core/Inc/MPU6050.h
+++ MPU6050_V1/Core/Inc/MPU6050.h
@@ -25,6 +25,11 @@
 #define ACCEL_ZOUT_L		0x40
 
 #define GYRO_XOUT_H			0x43
+#define GYRO_XOUT_L			0x44
+#define GYRO_YOUT_H			0x45
+#define GYRO_YOUT_L			0x46
+#define GYRO_ZOUT_H			0x47
+#define GYRO_ZOUT_L			0x48
 
 
 #define MPU6050_TIMEOUT 	500
