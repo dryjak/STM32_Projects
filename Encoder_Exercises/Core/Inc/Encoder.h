154c650ef0b597e8869c1776268ad101defba289
Jakub Dryla <135859429+dryjak@users.noreply.github.com>
2025-10-04 23:28:53 +0200

    added function to read angular velocity and angle of encoder

--- Encoder_Exercises/Core/Inc/Encoder.h
+++ Encoder_Exercises/Core/Inc/Encoder.h
@@ -13,15 +13,12 @@
 typedef struct
 {
 	TIM_HandleTypeDef *Tim;
-	uint16_t EncoderResolution;
+	uint16_t Resolution;
 	float SampleTime;
-
-	int16_t CurrentValue;
-	int16_t LastValue;
-	int16_t Delta;
 
 	float Angle;
 	float AngularVelocity;
+
 }Encoder_t;
 
 #endif /* INC_ENCODER_H_ */
--- Encoder_Exercises/Core/Src/Encoder.c
+++ Encoder_Exercises/Core/Src/Encoder.c
@@ -7,11 +7,32 @@
 
 #include "Encoder.h"
 
-void Init_Encoder(Encoder_t *Encoder, TIM_HandleTypeDef *Tim, uint16_t EncoderResolution, float SampleTime)
+void Encoder_Init(Encoder_t *Encoder, TIM_HandleTypeDef *Tim, uint16_t EncoderResolution, float SampleTime)
 {
 	Encoder->Tim = Tim;
-	Encoder->EncoderResolution = EncoderResolution;
+	Encoder->Resolution = EncoderResolution;
 	Encoder->SampleTime = SampleTime;
 
 	HAL_TIM_Encoder_Start(Encoder->Tim, TIM_CHANNEL_ALL);
+}
+
+void Encoder_AngularVelocity(Encoder_t *Encoder, float *EncoderAngle, float *EncoderAngularVelocity)
+{
+	int16_t CurrentValue =  __HAL_TIM_GetCounter(Encoder->Tim);
+	static int16_t LastValue = 0;
+
+	int16_t Delta = CurrentValue - LastValue;
+	if(Delta > (Encoder->Resolution) / 2)
+	{
+		Delta -=  (Encoder->Resolution);
+	}
+	else if(Delta < -(Encoder->Resolution) / 2)
+	{
+		Delta += (Encoder->Resolution);
+	}
+
+	*EncoderAngle = (360.0 * Delta) / (Encoder->Resolution);
+	*EncoderAngularVelocity = (*EncoderAngle) / (float)(Encoder->SampleTime);
+	LastValue = CurrentValue;
+
 }
