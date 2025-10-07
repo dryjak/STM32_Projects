9cfd233aa581f86cca6fa1880a76ce8d5954d199
Jakub Dryla <135859429+dryjak@users.noreply.github.com>
2025-10-07 15:27:11 +0200

    Added Functional init function

--- Encoder_Exercises/Core/Src/PID_Constant_Speed.c
+++ Encoder_Exercises/Core/Src/PID_Constant_Speed.c
@@ -4,5 +4,15 @@
  *  Created on: Oct 7, 2025
  *      Author: jakub
  */
+#include "PID_Constant_Speed.h"
 
+void Pid_Speed_Init(PID_Speed_t *Pid_Speed, float Kp, float Ki, float Kd, float dt, float OutputMax, float OutputMin)
+{
+	Pid_Speed->Kp = Kp;
+	Pid_Speed->Ki = Ki;
+	Pid_Speed->Kd = Kd;
 
+	Pid_Speed->dt = dt;
+	Pid_Speed->OutputMin = OutputMin;
+	Pid_Speed->OutputMax = OutputMax;
+}
--- Encoder_Exercises/Debug/Core/Src/PID_Constant_Speed.d
+++ Encoder_Exercises/Debug/Core/Src/PID_Constant_Speed.d
@@ -1,1 +1,3 @@
-Core/Src/PID_Constant_Speed.o: ../Core/Src/PID_Constant_Speed.c
+Core/Src/PID_Constant_Speed.o: ../Core/Src/PID_Constant_Speed.c \
+ ../Core/Inc/PID_Constant_Speed.h
+../Core/Inc/PID_Constant_Speed.h:
