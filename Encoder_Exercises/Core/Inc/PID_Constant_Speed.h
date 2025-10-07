1ab0ebc16f157d0751e419b9e2ef98f698e97721
Jakub Dryla <135859429+dryjak@users.noreply.github.com>
2025-10-07 15:21:46 +0200

    init function  sketch

--- Encoder_Exercises/Core/Inc/PID_Constant_Speed.h
+++ Encoder_Exercises/Core/Inc/PID_Constant_Speed.h
@@ -8,6 +8,21 @@
 #ifndef INC_PID_CONSTANT_SPEED_H_
 #define INC_PID_CONSTANT_SPEED_H_
 
+typedef struct
+{
+	float Kp;
+	float Ki;
+	float Kd;
 
+	float dt;
+	float IntegralValue;
+	float LastError;
+
+	float OutputMax;
+	float OutputMin;
+
+}PID_Speed_t;
+
+void Pid_Speed_Init(PID_Speed_t Pid_Speed, float Kp, float Ki, float Kd, float dt, float Integral, float LastError, float OutputMax, float OutputMin);
 
 #endif /* INC_PID_CONSTANT_SPEED_H_ */
