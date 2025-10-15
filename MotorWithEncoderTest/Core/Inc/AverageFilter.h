ec303e1883470310de58206ed5a0271742a6b21f
Jakub Dryla <135859429+dryjak@users.noreply.github.com>
2025-10-15 21:02:31 +0200

    struct for average

--- MotorWithEncoderTest/Core/Inc/AverageFilter.h
+++ MotorWithEncoderTest/Core/Inc/AverageFilter.h
@@ -10,4 +10,14 @@
 
 #include "main.h"
 
+#define MovingAverageLength 50
+
+typedef struct
+{
+	float buffer[MovingAverageLength];
+	double Sum;
+	uint8_t CurrentSample;
+	float Output;
+}Average_t;
+
 #endif /* INC_AVERAGEFILTER_H_ */
