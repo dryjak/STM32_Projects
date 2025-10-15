2b85176e686009ec398f71f3eda5d6592d7c36fd
Jakub Dryla <135859429+dryjak@users.noreply.github.com>
2025-10-15 20:46:38 +0200

    added liblary to calculate average

---
+++ MotorWithEncoderTest/Core/Inc/AverageFilter.h
@@ -0,0 +1,13 @@
+/*
+ * AverageFilter.h
+ *
+ *  Created on: Oct 15, 2025
+ *      Author: jakub
+ */
+
+#ifndef INC_AVERAGEFILTER_H_
+#define INC_AVERAGEFILTER_H_
+
+#include "main.h"
+
+#endif /* INC_AVERAGEFILTER_H_ */
---
+++ MotorWithEncoderTest/Core/Src/AverageFilter.c
@@ -0,0 +1,9 @@
+/*
+ * AverageFilter.c
+ *
+ *  Created on: Oct 15, 2025
+ *      Author: jakub
+ */
+
+
+#include "AverageFilter.h"
